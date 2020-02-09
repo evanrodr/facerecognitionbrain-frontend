a4a9b2f44e6f5c31dad6a5d18e3d1c73f0b04742
Evandro Rodrigues <43556689+evanrodr@users.noreply.github.com>
2020-02-08 23:30:31 -0300

    some changes to run everywhere with specific modifications

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('https://pacific-depths-61509.herokuapp.com/imageurl', {
+    fetch('http://localhost:3000/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -91,7 +91,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://pacific-depths-61509.herokuapp.com/image', {
+          fetch('http://localhost:3000/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
